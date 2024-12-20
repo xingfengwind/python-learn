05c3a764d7e9232a4ce0df7f3afe4b42223de91f
windnew <fr060116@outlook.com>
2024-12-20 20:38:02 +0800

    Changes

--- text.py
+++ text.py
@@ -3,7 +3,11 @@
 #定义2数相加的函数，通过参数接受被计算的2个数字
 def add(x,y):
     result = x + y
-    print(f"{x}+{y}的计算计算结果是：{result}")
+    #通过返回值结果，将相加的结果返回给调用者
+    return  result
+
+
 
 # 调用函数，传入被计算的2个字符
-add(5,6);
+r=add(5,6)
+print(r)
