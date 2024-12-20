bbe770f9eacdf79c51be958bdaebbb5437ce3c25
windnew <fr060116@outlook.com>
2024-12-20 21:23:18 +0800

    Changes2

--- text.py
+++ text.py
@@ -1,13 +1,29 @@
+def say_hi():
+    print("你好啊")
 
+result = say_hi()
+print(f"无返回值函数，返回内容是：{result}")
+print(f"无法返回值函数，返回内容类型是：{type(result)}")
 
-#定义2数相加的函数，通过参数接受被计算的2个数字
-def add(x,y):
-    result = x + y
-    #通过返回值结果，将相加的结果返回给调用者
-    return  result
+# 主动返回None的函数
+def say_hi2():
+    print("你好啊")
+    return None
 
+result = say_hi2()
+print(f"无返回值函数，返回内容是：{result}")
+print(f"无法返回值函数，返回内容类型是：{type(result)}")
 
+# None用于if判断
+def chack_age(age):
+    if age >= 18:
+        return "SUCCESS"
+    else:
+        return None
 
-# 调用函数，传入被计算的2个字符
-r=add(5,6)
-print(r)
+result = chack_age(16)
+if not result:
+    print("未成年，不可以进入 ")
+
+# None用于声明无初始内容的变量
+name=None
