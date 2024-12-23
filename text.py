e095d373063e0f4bbda91616fb53abaa6191a32b
windnew <fr060116@outlook.com>
2024-12-23 20:30:09 +0800

    Changes8

--- text.py
+++ text.py
@@ -1,31 +1,29 @@
- # 定义一个链表List
-my_list=["fr","wl","wx"]
-print(my_list)
-print(type(my_list))
-
-my_list=["ndh",666,True]
-print(my_list)
-print(type(my_list))
-
-# 定义一个嵌套列表
-my_list=[[1,2,3],[4,5,6]]
-print(my_list)
-print(type(my_list))
-# 通过下标索引取出对应位置的数据
-my_list=["Tom","Lily","Rose"]
-# 列表[下标索引]，从前向后从0开始，每次+1，从后向前从-1开始，每次-1
-print(my_list[0])
-print(my_list[1])
-print(my_list[2])
-
-# 列表[下标索引]，通过下标索引取数据，一定不要超出范围
-#print(my_list[3])
-# 通过下标索引取出数据(倒序取出)
-print(my_list[-1])
-print(my_list[-2])
-print(my_list[-3])
-
-# 取出嵌套列表的元素
-my_list=[[1,2,3],[4,5,6]]
-print(my_list[1][1])
-
+mylist=["cyuyan","java","python"]
+# 1.1 查找某元素
+index=mylist.index("cyuyan")
+print(f"cyuyan在列表中的下标索引值是：{index}")
+# 1.2如果被查找的元素不存在，会报错
+# index=mylist.index("hello")
+#  print(f"hello在列表中的下标索引值是：{index}“)
+# 2. 修改特定下标索引的值
+mylist[0]="c++"
+print(f"列表被修改值后，结果是：{mylist}")
+# 3.在指定下标位置插入新元素
+mylist.insert(1,"go")
+print(f"列表插入元素后，结果是：{mylist}")
+# 4。在列表的尾部追加```单个```新元素
+mylist.append("c#")
+print(f"追加元素后，结果是：{mylist}")
+# 5.在列表尾部追加```一批```新元素
+mylist2=[1,2,3]
+mylist.extend(mylist2)
+print(f"列表再追加一批新元素的列表后，结果是：{mylist}")
+# 6.删除指定下标索引的元素(两种方式)
+mylist=["a","b","c"]
+# 6.1 方式一：del 列表[下标]
+del mylist[2]
+print(f"列表删除元素后结果是：{mylist}")
+# 6.2方式2：列表。pop(下标)
+mylist=["a","b","c"]
+element=mylist.pop(2)
+print(f"通过pop方法取出元素后列表内容：{mylist}，取出元素：{element}")
