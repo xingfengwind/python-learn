dd0ce56c084270d0d5eaa88b85d59e15cb6e3eb9
windnew <fr060116@outlook.com>
2025-01-09 20:38:36 +0800

    Changes14

--- text.py
+++ text.py
@@ -1,12 +1,44 @@
-my_list = [1, 2, 3]
-# 定义一个空集合
-my_set=set()
+ # 定义一个字典
+my_dict1={"王力宏":99,"周杰伦":88,"李俊杰":77}
+# 定义空字典
+my_dict2={}
+my_dict3=dict()
+print(f"字典1的内容是:{my_dict1},类型：{type(my_dict1)}")
+print(f"字典1的内容是:{my_dict2},类型：{type(my_dict2)}")
+print(f"字典1的内容是:{my_dict3},类型：{type(my_dict3)}")
 
-#通过for循环遍历列表
-for element in my_list:
-    #在for循环中将列表的元素添加至集合
-    my_set.add(element)
+# 定义重复Key的字典
+my_dict1={"王力宏":99,"王力宏":88,"李俊杰":77}
+print(f"重复key的字典的内容是:{my_dict1}")
 
-# 最终得到的元素去重后的集合对象，并打印输出
-print(f"列表的内容是: {my_list}")
-print(f"通过for循环后,得到的集合对像是：{my_set}")
+# 从字典中基于Key获取Value
+my_dict1={"王力宏":99,"周杰伦":88,"李俊杰":77}
+score=my_dict1["王力宏"]
+print(f"王力宏的考试分数是:{score}")
+score=my_dict1["周杰伦"]
+print(f"周杰伦的考试分数是:{score}")
+# 定义嵌套字典
+stu_score_dict={
+    "王力宏":{
+        "语文":77,
+        "数学":86,
+        "英语":33
+    },
+    "周杰伦":{
+        "语文":88,
+        "数学":86,
+        "英语":55
+    },
+    "李俊杰":{
+        "语文":99,
+        "数学":96,
+        "英语":66
+    }
+}
+print(f"学生的考试信息:{stu_score_dict}")
+# 从嵌套字典中获取数据
+# 看一下周杰伦的语文信息
+score=stu_score_dict["周杰伦"]["语文"]
+print(f"周杰伦的语文成绩:{score}")
+score=stu_score_dict["李俊杰"]["英语"]
+print(f"李俊杰的英语成绩:{score}")
