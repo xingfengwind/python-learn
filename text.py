65d1d0c5ef8bddaeefd5b7c5fed8bb5d9aa5540c
windnew <fr060116@outlook.com>
2025-01-09 19:36:02 +0800

    Changes11

--- text.py
+++ text.py
@@ -1,33 +1,12 @@
-my_str="hhvghghejhgjgjh"
-# 通过下标索引取值
-value=my_str[2]
-value2=my_str[-6]
-print(f"从字符串{my_str}取下标为2的元素，值是：{value},去下标为-16的元素，值是：{value2}")
-# index方法：
-value =my_str.index("e")
-print(f"在字符串{my_str}中查找额，其起始下标是:{value}")
-# replace方法
-new_my_str=my_str.replace("e","2")
-print(f"将字符串{my_str}进行split切分后得到:{new_my_str}")
+my_list = [1, 2, 3]
+# 定义一个空集合
+my_set=set()
 
-# split方法
-my_str="ddg"
-my_str_list=my_str.split(" ")
-print(f"将字符串{my_str}进行split切分后得到:{my_str_list},类型是:{type(my_str_list)}")
+#通过for循环遍历列表
+for element in my_list:
+    #在for循环中将列表的元素添加至集合
+    my_set.add(element)
 
-# strip方法
-my_str=" ddd "
-new_my_str=my_str.strip()# 不传入参数，去除首尾空格
-print(f"字符串{my_str}被strip后，结果：:{new_my_str}")
-
-my_str="12ddd21"
-new_my_st=my_str.strip("12")
-print(f"字符串{my_str}被strip('12')后，结果：:{new_my_str}")
-
-#  统计字符串中某个字符的出现次数，count
-my_str="sjdjkednfkedj"
-count=my_str.count("j")
-print(count)
-# 统计字符串的长度，len()
-num=len(my_str)
-print(num)
+# 最终得到的元素去重后的集合对象，并打印输出
+print(f"列表的内容是: {my_list}")
+print(f"通过for循环后,得到的集合对像是：{my_set}")
