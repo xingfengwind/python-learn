3a04eb323351bc5e3bfc3e0089db5cf8031ce6cd
windnew <fr060116@outlook.com>
2025-02-16 16:07:23 +0800

    类13

--- diderjieduan.py
+++ diderjieduan.py
@@ -1,10 +1,73 @@
 """
-演示Union联合类型注解
+演示面向对象的多态特性以及抽象类（接口）的使用
 """
-# 使用Union类型，必须先导包
-from typing import Union
-my_list:list[Union[int,str]] = [1,2,"jdsaf","fjdsa"]
+class Animal:
+    def speak(self):
+        pass
 
-def func(data:Union[int,str]) -> Union[int,str]:
-    pass
-func()
+class Dog(Animal):
+    def speak(self):
+        print("汪汪汪")
+
+class Cat(Animal):
+    def speak(self):
+        print("喵喵喵")
+
+def make_noise(animal: Animal):
+    """制造点噪音，需要传入Animal对象"""
+    animal.speak()
+
+# 演示多态：使用2个子类对象来调用函数
+dog = Dog()
+cat = Cat()
+
+make_noise(dog)
+make_noise(cat)
+
+
+# 演示抽象类
+class AC:
+    def cool_wind(self):
+        """制冷"""
+        pass
+    def hot_wind(self):
+        """制热"""
+        pass
+    def swing_l_r(self):
+        """左右摆风"""
+        pass
+
+class Midea_AC(AC):
+    def cool_wind(self):
+        print("美的空调制冷")
+    def hot_wind(self):
+        print("美的空调制热")
+    def swing_l_r(self):
+        print("美的空调左右摆风")
+
+
+
+class GREE_AC(AC):
+
+
+    def cool_wind(self):
+         print("格力空调制冷")
+
+
+    def hot_wind(self):
+        print("格力空调制热")
+
+
+    def swing_l_r(self):
+        print("格力空调左右摆风")
+
+def make_cool(ac: AC):
+    ac.cool_wind()
+
+midea_ac = Midea_AC()
+gree_ac = GREE_AC()
+
+make_cool(midea_ac)
+make_cool(gree_ac)
+
+
